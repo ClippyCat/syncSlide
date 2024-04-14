15fc37992a77442e3607eaa06358c74da57cfe0d
melody580085 <melody580085@gmail.com>
2024-04-13 19:19:50 -0600

    goto

--- js/handlers.js
+++ js/handlers.js
@@ -1,27 +1,29 @@
 const updateRender = async () => {
-  const htmlDiv = document.getElementById("currentSlide");
-  renderMathInElement(htmlDiv, {
-    delimiters: [
-      {left: "$$", right: "$$", display: true},
-      {left: "$", right: "$", display: false}
-    ],
-    throwError: false,
-  });
+	const htmlDiv = document.getElementById("currentSlide");
+	renderMathInElement(htmlDiv, {
+		delimiters: [
+			{left: "$$", right: "$$", display: true},
+			{left: "$", right: "$", display: false}
+		],
+		throwError: false,
+	});
 }
 
 const renderHTML = async () => {
-  const md = new remarkable.Remarkable({
-    html: true,
-  });
-  const markdownInput = document.getElementById("markdown-input");
-  const mkInputAr = markdownInput.value.replaceAll("ARS", "<span lang=\"ar\" dir=\"rtl\">")
-  const mkInputAr2 = mkInputAr.replaceAll("ARE", "</span>");
-  getSlide = mkInputAr2.split("## ", 0);
+	const md = new remarkable.Remarkable({
+		html: true,
+	});
+	const markdownInput = document.getElementById("markdown-input").value;
+	const htmlOutput = document.getElementById("currentSlide");
+	getSlide = markdownInput.split("\n## ");
+	numSlides = getSlide.length;
+	goto = document.getElementById("goTo");
+	goto.max = numSlides;
+slideIndex = goto.value;
 
-  const htmlOutput = document.getElementById("currentSlide");
-  const newHtml = md.render(getSlide);
-  htmlOutput.innerHTML = newHtml;
-  updateRender();
+	const newHtml = md.render("## "+getSlide[slideIndex]);
+	htmlOutput.innerHTML = newHtml;
+	updateRender();
 }
 
 
