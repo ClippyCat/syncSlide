42602044d4aa27b21b7d77d51704598f5336edef
melody580085 <melody580085@gmail.com>
2024-06-12 13:22:43 -0600

    fix json

--- src/js/play.js
+++ src/js/play.js
@@ -2,16 +2,6 @@ const video = document.getElementById('myVideo');
 const rate = document.getElementById('rate');
 const slidesContainer = document.getElementById('currentSlide');
 
-async function fetchSlidesData() {
-const response = await fetch('recording.json');
-if (!response.ok) {
-throw new Error('Network response was not ok ' + response.statusText);
-}
-return await response.json();
-}
-
-const slidesData = fetchSlidesData();
-
 let ACTIVE_CONTENT_IDX = -1;
 video.ontimeupdate = (event) => {
 	const newActiveIndex = slidesData.findLastIndex((sd) => sd.time <= video.currentTime);
