8071a65a7173fe882fb05aaef5eee4739230dec7
melody580085 <melody580085@gmail.com>
2024-06-12 20:32:58 -0600

    fix foreach

--- src/js/play.js
+++ src/js/play.js
@@ -5,12 +5,12 @@ const goTo = document.getElementById("goTo");
 const go = document.getElementById("go");
 
 goTo.innerHTML = "<option value=0>Start: 0</option>";
-slidesData.forEach(e, i) => {
+slidesData.forEach((e, i) => {
 	const newOption = document.createElement('option');
 	newOption.value = e.time;
 	newOption.innerText = e.title + ": " + e.time;
 	goTo.appendChild(newOption);
-}
+});
 
 let ACTIVE_CONTENT_IDX = -1;
 video.ontimeupdate = (event) => {
