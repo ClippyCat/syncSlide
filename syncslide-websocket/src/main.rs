7893d45d33f662b9049bc0b5c8eeee484d048b58
Tait Hoyem <tait@tait.tech>
2025-11-07 18:58:59 -0700

    Add websocket docs, fix static issues

--- syncslide-websocket/src/main.rs
+++ syncslide-websocket/src/main.rs
@@ -1,3 +1,11 @@
+//! `syncslide-websocket`
+//!
+//! Runs the backend of the `SyncSlide` project.
+//!
+//! Handles live web-sockets (updating of slides live), as well as templated-HTML for most pages.
+//!
+#![deny(clippy::all, clippy::pedantic, rustdoc::all, unsafe_code, missing_docs)]
+
 use axum::{
     Router,
     extract::{
@@ -7,6 +15,7 @@ use axum::{
     response::{Html, Response},
     routing::get,
 };
+use futures_lite::future::or;
 use futures_util::{SinkExt, StreamExt};
 use tera::{Context, Tera};
 use tower_http::services::ServeDir;
@@ -15,7 +24,7 @@ use tokio::sync::broadcast::{self, Receiver, Sender};
 
 use serde::{Deserialize, Serialize};
 
-use signal_hook::consts::signal::*;
+use signal_hook::consts::signal::SIGUSR1;
 use signal_hook_tokio::Signals;
 
 use std::{
@@ -23,23 +32,36 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+/// A message indicating a _change_ in [`Presentation`] state.
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(tag = "type", content = "data")]
 #[serde(rename_all = "lowercase")]
 pub enum SlideMessage {
+    /// Change the `content` field of the presentation.
     Text(String),
+    /// Change the `slide` index.
     Slide(u32),
 }
 
+/// A specific presetation.
+///
+/// There is no key here, as presentations are stored in a hashmap with its associated keys.
 pub struct Presentation {
+    /// The full content of _all_ slides in the presentation.
     content: String,
+    /// The slide index that is currently active.
     slide: u32,
+    /// A set of channels for reading and writing to the sockets.
     channel: (Sender<SlideMessage>, Receiver<SlideMessage>),
 }
 
+/// The state of the entire application.
 #[derive(Clone, Default)]
 pub struct AppState {
+    /// Used to render HTML templates.
     tera: Arc<Tera>,
+    /// Used to store all the ongoing presentation.
+    /// They Key here is a user-defined string, and the value is a [`Presentation`] struct.
     slides: Arc<Mutex<HashMap<String, Arc<Mutex<Presentation>>>>>,
 }
 
@@ -51,9 +73,9 @@ async fn broadcast_to_all(
     ws.on_upgrade(|socket| ws_handle(socket, pid, state))
 }
 
-fn update_slide(pid: String, msg: SlideMessage, state: &mut AppState) {
+fn update_slide(pid: &str, msg: SlideMessage, state: &mut AppState) {
     let mut slides = state.slides.lock().unwrap();
-    let mut pres = slides.get_mut(&pid).unwrap().lock().unwrap();
+    let mut pres = slides.get_mut(pid).unwrap().lock().unwrap();
     match msg {
         SlideMessage::Slide(sn) => {
             pres.slide = sn;
@@ -61,36 +83,31 @@ fn update_slide(pid: String, msg: SlideMessage, state: &mut AppState) {
         SlideMessage::Text(text) => {
             pres.content = text;
         }
-    };
+    }
 }
 
 fn add_client_handler_channel(pid: String, state: &mut AppState) -> Arc<Mutex<Presentation>> {
-    let mut slides = if let Ok(slides) = state.slides.lock() {
-        slides
-    } else {
+    let Ok(mut slides) = state.slides.lock() else {
         // TODO: no panics
         panic!("Unable to lock K/V store!");
     };
     let pres = slides
         .entry(pid)
         .or_insert(Arc::new(Mutex::new(Presentation {
-            content: "".to_string(),
+            content: String::new(),
             slide: 0,
             channel: broadcast::channel(1024),
         })));
-    return Arc::clone(&pres);
+    Arc::clone(pres)
 }
 
-async fn handle_socket(
+fn handle_socket(
     msg: Result<Message, axum::Error>,
-    pid: String,
+    pid: &str,
     tx: &mut Sender<SlideMessage>,
-    mut state: &mut AppState,
+    state: &mut AppState,
 ) -> Result<bool, &'static str> {
-    let msg = if let Ok(msg) = msg {
-        msg
-    // disconnecteed
-    } else {
+    let Ok(msg) = msg else {
         return Err("Disconnected");
     };
     if let Message::Close(_) = msg {
@@ -98,18 +115,18 @@ async fn handle_socket(
     }
     let msg: SlideMessage = match serde_json::from_str(msg.to_text().unwrap()) {
         Ok(msg) => msg,
-        Err(e) => {
+        Err(_e) => {
             // TODO: proper error handling
             return Err("Invalid message!");
         }
     };
-    update_slide(pid.clone(), msg.clone(), &mut state);
+    update_slide(pid, msg.clone(), state);
 
     if tx.send(msg).is_err() {
         // disconnected
         return Err("Channel disconnected!");
     }
-    return Ok(true);
+    Ok(true)
 }
 
 async fn ws_handle(mut socket: WebSocket, pid: String, mut state: AppState) {
@@ -128,23 +145,19 @@ async fn ws_handle(mut socket: WebSocket, pid: String, mut state: AppState) {
     let (mut sock_send, mut sock_recv) = socket.split();
     let socket_handler = async {
         while let Some(msg) = sock_recv.next().await {
-            if handle_socket(msg, pid.clone(), &mut tx, &mut state1)
-                .await
-                .is_err()
-            {
+            if handle_socket(msg, &pid, &mut tx, &mut state1).is_err() {
                 return;
             }
         }
     };
     let channel_handler = async {
         while let Ok(msg) = rx.recv().await {
-            update_slide(pid.clone(), msg.clone(), &mut state);
+            update_slide(&pid, msg.clone(), &mut state);
             let text = serde_json::to_string(&msg).unwrap();
             sock_send.send(Message::from(text)).await.unwrap();
         }
     };
-    use futures_lite::future::or;
-    let _ = or(socket_handler, channel_handler).await;
+    let () = or(socket_handler, channel_handler).await;
 }
 
 async fn join(State(st): State<AppState>) -> Html<String> {
@@ -176,7 +189,7 @@ async fn index(State(st): State<AppState>) -> Html<String> {
 /// TODO: more effective solution via counting the number of _other_ connections each time a client
 /// is disconnected, and freeing the memory then.
 /// This would be vastly more efficent and scale better.
-async fn cleanup(state: AppState) {
+fn cleanup(state: &mut AppState) {
     let mut slides = state.slides.lock().unwrap();
     slides.retain(|_k, v| Arc::strong_count(v) == 1);
 }
@@ -184,9 +197,9 @@ async fn cleanup(state: AppState) {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     // USR1 signal causes cleanup routine
-    let sig_handle = Signals::new(&[SIGUSR1]).unwrap().handle();
+    let sig_handle = Signals::new([SIGUSR1]).unwrap().handle();
     let tera = Tera::new("templates/**/*.html").unwrap();
-    let state = AppState {
+    let mut state = AppState {
         tera: Arc::new(tera),
         slides: Arc::new(Mutex::new(HashMap::new())),
     };
@@ -202,7 +215,7 @@ async fn main() {
         .nest_service("/demo", ServeDir::new("../src/demo/"))
         .with_state(state.clone());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5002").await.unwrap();
-    let signal_task = tokio::spawn(cleanup(state));
+    let signal_task = tokio::spawn(async move { cleanup(&mut state) });
     axum::serve(listener, app).await.unwrap();
     sig_handle.close();
     let _ = signal_task.await;
